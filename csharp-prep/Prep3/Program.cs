e27f89d426a91648c3e4dc7d02b6b94178b186ed
epatten23 <epatten@byui.edu>
2024-01-16 13:23:08 -0700

    prep3n4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random magic = new Random();
+        int magicNum = magic.Next(26);
+        Console.WriteLine("What is your guess?(Between 0-25)");
+        string guess = Console.ReadLine();
+        int guessNum = int.Parse(guess);
+        int guesses = 0;
+        while(magicNum != guessNum){
+        if(magicNum > guessNum){
+            Console.WriteLine("Higher");
+            Console.WriteLine("What is your guess?(Between 0-25)");
+            guess = Console.ReadLine();
+            guessNum = int.Parse(guess);
+        }
+        if(magicNum < guessNum){
+            Console.WriteLine("Lower");
+            Console.WriteLine("What is your guess?(Between 0-25)");
+            guess = Console.ReadLine();
+            guessNum = int.Parse(guess);
+            }
+         guesses += 1;
+        }
+        Console.WriteLine($"Congrats! The magic number was {magicNum}");
+        Console.WriteLine($"It took you {guesses} tries to guesss the number");
     }
+
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
+using System.Transactions;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int amt = 0;
+        int largeNum = 0;
+        int number = 0;
+        do{
+        Console.WriteLine("Enter as number (Enter 0 to stop): ");
+        string strnumber = Console.ReadLine();
+        number = int.Parse(strnumber);
+        numbers.Add(number);
+        }
+        while(number != 0);
+        foreach(int num in numbers){
+            sum += num;
+            amt += 1; 
+            if (num>largeNum){
+                largeNum = num;
+            }
+        };
+        
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {sum/(amt-1)}");
+        Console.WriteLine($"The largest number is {largeNum}");
+        
     }
 }
