8f021da909ec42bd0225b3563537dc61b1a09d27
epatten23 <epatten@byui.edu>
2024-01-16 19:46:55 -0700

    final prep push

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNum = PromptNumber();
+        int squarNum = SquareNumber(userNum);
+
+        DisplayResult(userName, squarNum);
+
+    }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName(){
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptNumber(){
+        Console.Write("What's your favorite number?: ");
+        string number = Console.ReadLine();
+        int userNum = int.Parse(number);
+
+        return userNum;
+    }
+    static int SquareNumber(int number){
+        int num = number * number;
+        return num;
+    }
+    static void DisplayResult(string name, int numb){
+        Console.WriteLine($"Hello {name}, the square of your favorite number is {numb}");
     }
 }
