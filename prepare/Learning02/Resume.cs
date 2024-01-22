aaeb2102ee92aff4d8dd0f8e7815f9927d1c1605
epatten23 <epatten@byui.edu>
2024-01-22 12:22:28 -0700

    learning02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+
+public class Resume {
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach(Job position in _jobs){
+            position.Display();
+        };
+    }
+}
+
